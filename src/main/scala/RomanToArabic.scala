e4fed5c9ead4452af691a08cb61007009b9b8d22
kenneth.rayner <kenneth.rayner@digital.hmrc.gov.uk>
2020-04-23 08:47:29 +0100

    refactor

--- src/main/scala/RomanToArabic.scala
+++ src/main/scala/RomanToArabic.scala
@@ -14,11 +14,17 @@ object RomanToArabic {
     val foo: Seq[Char] = romanNumeral.toList
     val nums: Seq[Int] = foo.map(x => romanNum(x))
 
-    val sumTuple: (Int, Int) =  nums.foldLeft((0,0)) {
-      case ((acc, previousNUmber), currentNumber) =>
-        (acc + (if (previousNUmber < currentNumber) -(2*previousNUmber) else 0)  + currentNumber, currentNumber) }
+//    val sumTuple: (Int, Int) =  nums.foldLeft((0,0)) {
+//      case ((acc, previousNUmber), currentNumber) =>
+//        (acc + (if (previousNUmber < currentNumber) -(2*previousNUmber) else 0)  + currentNumber, currentNumber) }
+//
+//    sumTuple._1
 
-    sumTuple._1
+          nums.foldLeft((0,0)) {
+          case ((acc, previousNUmber), currentNumber) =>
+            (acc + (if (previousNUmber < currentNumber) -(2*previousNUmber) else 0)  + currentNumber, currentNumber) }._1
+
+
 
 
     }
--- src/main/scala/untitled.sc
+++
@@ -1,4 +0,0 @@
-val foo = List(1,2,3,4)
-val p = foo.length
-
-
